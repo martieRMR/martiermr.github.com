ac2e874eac830d8f91641c81f888ec01f3b6949b
pixL <75906210+phtps123@users.noreply.github.com>
2021-11-07 16:08:49 -0300

    initial release

---
+++ boletines/javascript/main.js
@@ -0,0 +1,33 @@
+
+/*
+
+let links = document.querySelectorAll('a');
+
+links.forEach()
+
+*/
+
+let links = document.querySelectorAll('.close');
+
+//PARA CADA ELEMENTO QUE ESTE DENTRO DEL ARREGLO DE CLOSEBTN, REALIZAR ESTO (FUNCION)
+
+links.forEach(function(ev){
+    ev.addEventListener('click',function(ev){
+        ev.preventDefault();
+
+        let content = document.querySelector('.content');
+
+
+        content.classList.remove('animate__fadeInDown');
+        content.classList.remove('animate__animated');
+
+        content.classList.add('animate__fadeOutUp');
+        content.classList.add('animate__animated');
+        
+        setTimeout(function(){
+            location.href = '../index.html';
+        },600)
+
+        
+    })
+})
